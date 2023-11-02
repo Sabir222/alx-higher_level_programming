1a71e3f80df4786059ddbc6eb2cf8b40d55a9c11
Sabir222 <mrsabir4@gmail.com>
2023-11-02 19:20:54 +0100

    ex8

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-from printfile import printFunction; printFunction()
-
+import printfile
--- 0x02-python-import_modules/printfile.py
+++ 0x02-python-import_modules/printfile.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-
-def printFunction():
-    print("#pythoniscool")
+print("#pythoniscool")

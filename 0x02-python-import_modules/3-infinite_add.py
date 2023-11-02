e16c82ffc8ad7259ef9cf4a66317c06ac78f1fdc
Sabir222 <mrsabir4@gmail.com>
2023-11-02 15:39:23 +0100

    ex4

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,10 +4,7 @@
 if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) == 1:
-        print("0")
-    else:
-        result = 0
-        for i in range(1, len(sys.argv)):
-            result += int(sys.argv[i])
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
     print("{}".format(result))

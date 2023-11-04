bf96b7470aa2335715c8d34be5001747ed8520e1
Sabir222 <mrsabir4@gmail.com>
2023-11-04 16:08:06 +0100

    ex9

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+def multiple_returns(sentence):
+    if sentence is None:
+        return None, None
+    elif len(sentence) == 0:
+        return 0, None
+    else:
+        return len(sentence), sentence[0]

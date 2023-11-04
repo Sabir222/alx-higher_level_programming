67f4dfbcae47832e80f895b2657dc83609b62d9f
Sabir222 <mrsabir4@gmail.com>
2023-11-04 09:59:36 +0100

    ex

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
+
 def print_matrix_integer(matrix=[[]]):
-   s 
+    for i in matrix:
+        for j in i:
+            print("{}".format(j), end=" ")
+        print()
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_b) == 0:
+        return tuple_a
+    elif len(tuple_b) == 1:
+        return (tuple_a[0] + tuple_b[0], tuple_a[1])
+    else:
+        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])

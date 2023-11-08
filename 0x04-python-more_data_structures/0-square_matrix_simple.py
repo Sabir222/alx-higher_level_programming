a3801d452ba444e20e06dd00bbe497d6287175d7
Sabir222 <mrsabir4@gmail.com>
2023-11-08 10:42:57 +0100

    Worked on: 0x04-python-more_data_structures/0-square_matrix_simple.py 0x04-python-more_data_structures/__pycache__/0-square_matrix_simple.cpython-310.pyc

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-
-
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squaredMatrix = []
-    for i in matrix:
-        row = [j**2 for j in i]
-        squaredMatrix.append(row)
-    return squaredMatrix
+    return [[y**2 for y in x] for x in matrix]
+
+
+# def square_matrix_simple(matrix=[]):
+#     squaredMatrix = []
+#     for i in matrix:
+#         row = [j**2 for j in i]
+#         squaredMatrix.append(row)
+#     return squaredMatrix

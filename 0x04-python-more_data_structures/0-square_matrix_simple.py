be3a10253236acd0ad0aa0815ef3294c548be05d
Sabir222 <mrsabir4@gmail.com>
2023-11-08 10:36:33 +0100

    Worked on: 0-main.py 0-square_matrix_simple.py __pycache__/0-square_matrix_simple.cpython-310.pyc

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,7 @@
 
 def square_matrix_simple(matrix=[]):
     squaredMatrix = []
-    
+    for i in matrix:
+        row = [j**2 for j in i]
+        squaredMatrix.append(row)
+    return squaredMatrix

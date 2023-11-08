b339a5536a76a3d784dba1b87136085e0b104e7e
Sabir222 <mrsabir4@gmail.com>
2023-11-08 11:02:51 +0100

    Worked on: 3-common_elements.py __pycache__/3-common_elements.cpython-310.pyc

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+# def common_elements(set_1, set_2):
+#     com_set = []
+#     for i in set_2:
+#         if i in set_1:
+#             com_set.append(i)
+#     return com_set
+
+
+# def common_elements(set_1, set_2):
+#     return [i for i in set_2 if i in set_1]
+
+
 def common_elements(set_1, set_2):
-    com_set = []
-    for i in set_2:
-        if i in set_1:
-            com_set.append(i)
-    return com_set
+    return set_1 & set_2
